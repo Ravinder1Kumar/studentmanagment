7d22f61502405d61f0470cd6524053f2e97d6646
nbsegit <nbsegit@tuespotsolutions.com>
2022-09-28 16:47:18 +0530

    student profile model added by ravinder

---
+++ src/com/demo/models/StudentProfile.java
@@ -0,0 +1,27 @@
+package com.demo.models;
+
+public class StudentProfile {
+	
+	private String studentProfile;
+
+	public StudentProfile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentProfile(String studentProfile) {
+		super();
+		this.studentProfile = studentProfile;
+	}
+
+	public String getStudentProfile() {
+		return studentProfile;
+	}
+
+	public void setStudentProfile(String studentProfile) {
+		this.studentProfile = studentProfile;
+	}
+	
+	
+
+}
